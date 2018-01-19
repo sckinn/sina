97acbbc357353aae5e78ecbfbb7770575bcade7e
sckinn <35490559+sckinn@users.noreply.github.com>
2018-01-19 15:08:43 +0800

    Update sina.py

--- sina/spiders/sina.py
+++ sina/spiders/sina.py
@@ -5,9 +5,10 @@ import json
 
 class SinaSpider(scrapy.Spider):
     name = "sina"
-    allowed_domains = ["dapp-beta2.degico.co","hq.sinajs.cn"]
+    allowed_domains = ["dapp-beta2.degico.co","dapp.cloudapp.net","hq.sinajs.cn"]
     start_urls = [
-        'https://dapp-beta2.degico.co/hkgdstock/getsrcurls',
+        #'https://dapp-beta2.degico.co/hkgdstock/getsrcurls',
+        'https://dapp.cloudapp.net/hkgdstock/getsrcurls',
     ]
 
     def parse(self, response):
