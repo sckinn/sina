b12f7d9e0b164693c84516fe8c24b9dfc69173d7
sckinn <35490559+sckinn@users.noreply.github.com>
2018-01-19 14:35:42 +0800

    Update settings.py

--- sina/settings.py
+++ sina/settings.py
@@ -8,3 +8,8 @@ NEWSPIDER_MODULE = 'sina.spiders'
 ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
 ROBOTSTXT_OBEY = False
+
+DOWNLOADER_MIDDLEWARES = {
+      'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' : None,
+      'Crawler.comm.rotate_useragent.RotateUserAgentMiddleware' :400
+}
